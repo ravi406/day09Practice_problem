8e745cc6992a7eab116eff31e7bcb7cb4d8f0418
ravi406 <ravikamble406@gmail.com>
2022-12-02 21:52:35 +0530

    Added : ContactDetails class to add new contact in AddressBook system

--- src/com/day9/oops/AddressBook.java
+++ src/com/day9/oops/AddressBook.java
@@ -4,5 +4,13 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        ContactDetails contactDetails = new ContactDetails();
+        contactDetails.setFirstName("Ravikumar");
+        contactDetails.setLastName("Kamble");
+        contactDetails.setAddress("Gandhi Nagar");
+        contactDetails.setCity("Jamkhandi");
+        contactDetails.setZip(587302);
+        contactDetails.setEmail("ravi123@gmail.com");
+        System.out.println(contactDetails.toString());
     }
 }
---
+++ src/com/day9/oops/ContactDetails.java
@@ -0,0 +1,97 @@
+package com.day9.oops;
+/*
+*  creating a new POJO class for contacDetails that contains variables and their getter's and setter's.
+*/
+public class ContactDetails {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /*
+    * toString() method is used to print the values of ContactDetails
+    */
+
+    @Override
+    public String toString() {
+        return "ContactDetails{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
